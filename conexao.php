2d7b35fe7cb102b76c671b451fc48f1a89533d4e
Guilherme Freire <65873804+Guilherme-bot149@users.noreply.github.com>
2022-05-04 09:43:41 -0300

    Delete conexao.php

--- conexao.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('HOST', 'IP DO BANCO DE DADOS');
-define('USUARIO',  'root');
-define('SENHA', 'root');
-define('DB', 'login');
-
-$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possivel conectar')
